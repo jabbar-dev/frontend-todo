3c42d35f5be45bd9b832398b17e2b6107bf2f821
jabbar-dev <rashdijabbar@gmail.com>
2022-07-18 16:01:19 +0500

    adds file

---
+++ todo-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Formik, Form, Field} from 'formik';
+import {Register} from './components/Register'
+
+function App() {
+  return (
+  
+   <Register/>
+  
+    )
+}
+
+export default App;
